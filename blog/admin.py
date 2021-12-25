5b51378c43321e6a08cde0c81ed7557314605374
Charline BOITEL <charline.boitel@capgemini.com>
2021-12-25 08:55:04 +0000

    finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 admin.site.register(Tag)
 
@@ -10,4 +10,4 @@ class PostAdmin(admin.ModelAdmin):
     
 
 admin.site.register(Post, PostAdmin)
-    
+admin.site.register(Comment)
